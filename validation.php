dc2da36f056b5985304532293ddaf4a0e7a22852
zhongsiyong <zhongsiyong@ycf.com>
2018-06-08 14:43:55 +0800

    update validate class

--- validation.php
+++ validation.php
@@ -7,25 +7,28 @@
  * Version: 1.0
  */
 
-function urlValidate($var){
-    echo filter_var($var,FILTER_VALIDATE_URL)?"yes,".$var." is a url\n":$var." is a invalid url\n";
-}
-function emailValidate($var){
-    echo filter_var($var,FILTER_VALIDATE_EMAIL)?"yes,".$var." is a email\n":$var." is a invalid email\n";
-}
-function ipValidate($var){
-    echo filter_var($var,FILTER_VALIDATE_IP)?"yes,".$var." is an ip\n":$var." is a invalid ip\n";
-}
-function intValidate($var){
-    echo filter_var($var,FILTER_VALIDATE_INT)?"yes,".$var." is a integer\n":$var." is a invalid integer\n";
+class filter{
+    public function urlValidate($var){
+        echo filter_var($var,FILTER_VALIDATE_URL)?"yes,".$var." is a url\n":$var." is a invalid url\n";
+    }
+    public function emailValidate($var){
+        echo filter_var($var,FILTER_VALIDATE_EMAIL)?"yes,".$var." is a email\n":$var." is a invalid email\n";
+    }
+    public function ipValidate($var){
+        echo filter_var($var,FILTER_VALIDATE_IP)?"yes,".$var." is an ip\n":$var." is a invalid ip\n";
+    }
+    public function intValidate($var){
+        echo filter_var($var,FILTER_VALIDATE_INT)?"yes,".$var." is a integer\n":$var." is a invalid integer\n";
+    }
 }
 
-urlvalidate("mine.sion.com");
-urlvalidate("http://mine.sion.com");
-emailvalidate("justin@qq.com");
-emailvalidate("isEmail.com");
-ipvalidate("127.0.0.1");
-ipvalidate("198.1");
-intvalidate("198.1");
-intvalidate("4");
-intvalidate("4.0");
+$filter = new filter();
+$filter->urlvalidate("mine.sion.com");
+$filter->urlvalidate("http://mine.sion.com");
+$filter->emailvalidate("justin@qq.com");
+$filter->emailvalidate("isEmail.com");
+$filter->ipvalidate("127.0.0.1");
+$filter->ipvalidate("198.1");
+$filter->intvalidate("198.1");
+$filter->intvalidate("4");
+$filter->intvalidate("4.0");
